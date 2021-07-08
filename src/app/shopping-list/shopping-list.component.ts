2cdf985e491361973e6483d3e639ba6a1b9ea12a
Andrew Palchykovsky <dangerok2@gmail.com>
2021-07-08 21:19:12 +0300

    Changed event emmiters to subject

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from './shopping-list.service';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from './shopping-list.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private igChangeSub: Subscription;
 
-
-  constructor(private shoppingList: ShoppingListService) { }
+  constructor(private shoppingList: ShoppingListService) {
+  }
 
   ngOnInit(): void {
     this.ingredients = this.shoppingList.getIngredients();
-    this.shoppingList.ingredientsChanged.subscribe(
+    this.igChangeSub = this.shoppingList.ingredientsChanged.subscribe(
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
     );
+  }
+
+  ngOnDestroy() {
+    this.igChangeSub.unsubscribe();
   }
 
 }
